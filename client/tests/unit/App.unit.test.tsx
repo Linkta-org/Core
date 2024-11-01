5ab460d253168eae09a1f93d63f7bc61afc6e6d7
Colin <colin.vandergraaf@gmail.com>
2024-11-01 14:39:45 -0700

    modify App unit test to work

--- client/tests/unit/App.unit.test.tsx
+++ client/tests/unit/App.unit.test.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import { render, screen } from '@utils/testUtils';
-import App from '@/App';
+import { vi } from 'vitest';
+import type { Mock } from 'vitest';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import useWatchAuthenticatedState from '@hooks/useWatchAuthenticatedState';
+import { getRoutes } from '@routes/index';
+
+vi.mock('@hooks/useWatchAuthenticatedState', () => ({
+  default: vi.fn(),
+}));
 
 describe('App', () => {
-  it('renders homepage content', () => {
-    render(<App />);
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-    expect(screen.getByText(/Revolutionizing Learning/i)).toBeInTheDocument();
+  const renderApp = (isAuthenticated: boolean) => {
+    (useWatchAuthenticatedState as Mock).mockReturnValue({
+      isAuthenticated,
+      isLoading: false,
+    });
+
+    const router = createMemoryRouter(getRoutes());
+    render(<RouterProvider router={router} />);
+  };
+
+  it('renders UnauthorizedLayout when not authenticated', () => {
+    renderApp(false);
+    expect(
+      screen.getByRole('heading', {
+        name: /revolutionizing learning: intuitive visualization for complex concepts/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it('renders MainLayout when authenticated', () => {
+    renderApp(true);
+    expect(screen.getByText('Explore a New Topic')).toBeInTheDocument();
   });
 });
